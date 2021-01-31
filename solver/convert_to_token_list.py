5df1aac9144e34f94bbfe056a915b30ff975b09d
hoanthiennguyen <hoanthien97@gmail.com>
2021-01-31 22:06:55 +0700

    convert to token list, prepare for parsing to postfix

---
+++ solver/convert_to_token_list.py
@@ -0,0 +1,54 @@
+import unittest
+
+
+def convert_to_token_list(expression):
+    result = []
+    for i in range(len(expression)):
+        character = expression[i]
+        if check_is_part_of_number(character) and len(result) > 0:
+            last_token = result[len(result) - 1]
+            if check_is_a_number(last_token):
+                result[len(result) - 1] = last_token + character
+            else:
+                result.append(character)
+        else:
+            result.append(character)
+    return result
+
+
+def check_is_part_of_number(character):
+    return character in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."]
+
+
+def check_is_a_number(token):
+    try:
+        float(token)
+        return True
+    except ValueError:
+        return False
+
+
+class Tests(unittest.TestCase):
+    
+    def test_check_is_part_of_digit(self):
+        self.assertEqual(check_is_part_of_number("5"), True)
+        self.assertEqual(check_is_part_of_number("0"), True)
+        self.assertEqual(check_is_part_of_number("9"), True)
+        self.assertEqual(check_is_part_of_number("."), True)
+        self.assertEqual(check_is_part_of_number("x"), False)
+
+    def test_check_is_a_number(self):
+        self.assertEqual(check_is_a_number("5"), True)
+        self.assertEqual(check_is_a_number("0"), True)
+        self.assertEqual(check_is_a_number("15"), True)
+        self.assertEqual(check_is_a_number("1.5"), True)
+        self.assertEqual(check_is_a_number("0.15"), True)
+
+    def test_convert_to_token_list(self):
+        self.assertEqual(convert_to_token_list("x"), ["x"])
+        self.assertEqual(convert_to_token_list("x+1"), ["x", "+", "1"])
+        self.assertEqual(convert_to_token_list("x+10"), ["x", "+", "10"])
+        self.assertEqual(convert_to_token_list("x^2+10"), ["x", "^", "2", "+", "10"])
+        self.assertEqual(convert_to_token_list("1.2*x^2+10"), ["1.2", "*", "x", "^", "2", "+", "10"])
+        self.assertEqual(convert_to_token_list("1.25*x^2+100"), ["1.25", "*", "x", "^", "2", "+", "100"])
+
